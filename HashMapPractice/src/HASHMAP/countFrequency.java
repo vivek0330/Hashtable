ffb1ba6979f7d96345b835351c23a0c25c807380
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-21 00:43:44 +0530

    find the Frequency of word in a sentence

--- HashMapPractice/src/HASHMAP/countFrequency.java
+++ HashMapPractice/src/HASHMAP/countFrequency.java
@@ -1,7 +1,28 @@
 package HASHMAP;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class countFrequency {
+	static void count_freq(String str) {
+		Hashtable<String, Integer> mp = new Hashtable<>();
+		String arr[] = str.split("\\s");
+
+		for (int i = 0; i < arr.length; i++) {
+			if (mp.containsKey(arr[i])) {
+				mp.put(arr[i], mp.get(arr[i]) + 1);
+			} else {
+				mp.put(arr[i], 1);
+			}
+		}
+
+		for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to the hash table Problem");
+		String str = "To be or not to be";
+		count_freq(str);
 	}
 }
