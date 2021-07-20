1abf174af8d5657e9f021788d356c1ee22c375a2
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-21 00:46:32 +0530

    find the Frequency of word in a large sentence

--- HashMapPractice/src/HASHMAP/countFrequency.java
+++ HashMapPractice/src/HASHMAP/countFrequency.java
@@ -22,7 +22,7 @@ public class countFrequency {
 	}
 
 	public static void main(String[] args) {
-		String str = "To be or not to be";
+		String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		count_freq(str);
 	}
 }
