160de09d07543cb86388b4989a42246f012216ee
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-22 01:36:59 +0530

    count frequency of words in a large sentence and remove key also

--- HashMapPractice/src/com/hashTable/HashTable.java
+++ HashMapPractice/src/com/hashTable/HashTable.java
@@ -1,26 +1,37 @@
 package com.hashTable;
 
 public class HashTable {
-    public static void main(String[] args) {
-        System.out.println("Welcome to HashTable program");
-        //Creating object as hashCode for LinkedList class
-        LinkedList<String, Integer> hashCode = new LinkedList<String, Integer>();
-        String message = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+	public static void main(String[] args) {
+		System.out.println("******************************************");
+		System.out.println("*    Welcome to the hash table program   *");
+		System.out.println("******************************************");
+		System.out.println("\n");
+		// Creating object as hashCode for LinkedList class
+		LinkedList<String, Integer> list = new LinkedList<String, Integer>();
+		String message = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-        //String to String[] conversion.
-        String[] messageArray = message.toLowerCase().split(" ");
-        //Here we are Iterating within the array
-          for(String word : messageArray) {
-            //Search for the word in the linked list
-            Integer value = hashCode.get(word);
-            if (value == null)
-                value = 1;
-             else
-                value = value + 1;
-                hashCode.add(word, value);
+		// String to String[] conversion.
+		String[] messageArray = message.toLowerCase().split(" ");
+		// Here we are Iterating within the array
+		for (String word : messageArray) {
+			// Search for the word in the linked list
+			Integer value = list.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			list.add(word, value);
 
-          }
-        //Display the linked list
-        System.out.println(hashCode);
-    }
+		}
+		// Display the linked list
+		System.out.println(list);
+		System.out.println("\n");
+
+		// Create remove method
+		list.replace("avoidable");
+		System.out.println("************************************");
+		System.out.println("*    After replace method call ::  *");
+		System.out.println("************************************");
+		System.out.println("\n" + list);
+	}
 }
--- HashMapPractice/src/com/hashTable/LinkedList.java
+++ HashMapPractice/src/com/hashTable/LinkedList.java
@@ -1,71 +1,109 @@
 package com.hashTable;
 
-public class LinkedList<K,V> {
-    MyMapNode<K, V> head;
-    MyMapNode<K, V> tail;
-    ///creating add method
-    public void add(K key, V value)
-    {
-        MyMapNode<K, V> myNewNode = (MyMapNode<K, V>) searchNode(key);
-        if(myNewNode == null)
-        {
-            myNewNode = new MyMapNode<>(key , value);
-            this.append(myNewNode);
-        }
-        else
-        {
-            myNewNode.setValue(value);
-        }
-    }
+import java.util.ArrayList;
 
-    //Append the value in the linked list
-    public void append(MyMapNode<K, V> myNode)
-    {
-        if(this.head == null)
-            this.head = myNode;
-        if(this.tail == null)
-            this.tail = myNode;
-        else
-        {
-            this.tail.setNext(myNode);
-            this.tail = myNode;
-        }
-    }
+public class LinkedList<K, V> {
+	MyMapNode<K, V> head;
+	MyMapNode<K, V> tail;
+	private final int numOfBuckets;
+	ArrayList<MyMapNode<K, V>> myBucketArray;
 
-    //Searching for the word in the linked list
-    public MyMapNode<K, V> searchNode(K data)
-    {
-        MyMapNode<K, V> currentNode = head;
-        int position = 0;
-        while (currentNode != null)
-        {
-            position++;
-            if (currentNode.getKey().equals(data))
-            {
-                return currentNode;
-            }
-            currentNode = currentNode.getNext();
-        }
-        return currentNode;
-    }
+	public LinkedList() {
+		this.numOfBuckets = 20;
+		this.myBucketArray = new ArrayList<>();
+		for (int i = 0; i < numOfBuckets; i++)
+			this.myBucketArray.add(null);
+	}
 
-    //here we are Searching for the word and getting the value from the linked list
-    public V get(K word)
-    {
-        MyMapNode<K, V> myMapNode = searchNode(word);
-        return (myMapNode == null) ? null : myMapNode.getValue();
-    }
+	// creating add method
+	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		MyMapNode<K, V> myNewNode = this.myBucketArray.get(index);
+		if (myNewNode == null) {
+			myNewNode = new MyMapNode<>(key, value);
+			this.myBucketArray.set(index, myNewNode);
+		}
+		myNewNode = (MyMapNode<K, V>) searchNode(key);
+		if (myNewNode == null) {
+			myNewNode = new MyMapNode<>(key, value);
+			this.append(myNewNode);
+		} else {
+			myNewNode.setValue(value);
+		}
+	}
 
-    //Print the linked list
-    @Override
-    public String toString()
-    {
-        return "MyLinkedListNodes{" + head + "}";
-    }
+	private int getBucketIndex(K word) {
+		int hashCode = Math.abs(word.hashCode());
+		int index = hashCode % numOfBuckets;
+		return index;
+	}
 
-    public void printNodes()
-    {
-        System.out.println("My nodes: " + head);
-    }
+	// Append the value in the linked list
+	public void append(MyMapNode<K, V> myNewNode) {
+		if (this.head == null)
+			this.head = myNewNode;
+		if (this.tail == null)
+			this.tail = myNewNode;
+		else {
+			this.tail.setNext(myNewNode);
+			this.tail = myNewNode;
+		}
+	}
+
+	// Searching for the word in the linked list
+	public MyMapNode<K, V> searchNode(K data) {
+		MyMapNode<K, V> currentNode = head;
+		@SuppressWarnings("unused")
+		int position = 0;
+		while (currentNode != null) {
+			position++;
+			if (currentNode.getKey().equals(data)) {
+				return currentNode;
+			}
+			currentNode = currentNode.getNext();
+		}
+		return currentNode;
+	}
+
+	// here we are Searching for the word and getting the value from the linked list
+	public V get(K word) {
+		int index = this.getBucketIndex(word);
+		@SuppressWarnings("unused")
+		MyMapNode<K, V> myNewNode = this.myBucketArray.get(index);
+		if (this.myBucketArray.get(index) == null)
+			return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) searchNode(word);
+		return (myMapNode == null) ? null : myMapNode.getValue();
+	}
+
+	// creating remove method
+	public void replace(K word) {
+
+		MyMapNode<K, V> currentNode = head;
+		MyMapNode<K, V> previousNode = null;
+		while (currentNode != null && currentNode.getKey().equals(word)) {
+			head = currentNode.getNext();
+			return;
+		}
+		while (currentNode != null && !(currentNode.getKey().equals(word))) {
+			previousNode = currentNode;
+			currentNode = currentNode.getNext();
+		}
+		if (currentNode != null) {
+			previousNode.next = currentNode.next;
+		}
+		if (currentNode == null)
+			System.out.println("Word not found!");
+	}
+
+	// Print the linked list
+	@Override
+	public String toString() {
+		return "MyLinkedListNodes{" + head + "}";
+	}
+
+	public void printNodes() {
+		System.out.println("My nodes: " + head);
+	}
 
 }
